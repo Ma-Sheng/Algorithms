ab3034db861d01f6a4c54471438a50a5ca1e7471
Ma-Sheng <1337632264@qq.com>
2018-05-28 20:32:12 +0800

    测试第二次提交

--- src/Code/Peoblem1.java
+++ src/Code/Peoblem1.java
@@ -1,5 +1,35 @@
 package Code;
 
 public class Peoblem1 {
+    // 测试一下
+    public static class Singleton {
+        private final static Singleton INSTANCE = new Singleton();
 
+        private Singleton() {
+
+        }
+
+        public static Singleton getInstance() {
+            return INSTANCE;
+        }
+    }
+
+    /**
+     * 单例模式，饿汉式，线程不安全
+     */
+    public static class Singleton2 {
+        private static Singleton2 instance = null;
+
+        private Singleton2() {
+
+        }
+
+        public static Singleton2 getInstance() {
+            if (instance == null) {
+                instance = new Singleton2();
+            }
+
+            return instance;
+        }
+    }
 }
