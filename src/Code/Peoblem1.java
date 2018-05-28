3957d2ec867ef4a7d770610c8d9f4cf43f2999e6
Ma-Sheng <1337632264@qq.com>
2018-05-28 20:34:34 +0800

    第三次

--- src/Code/Peoblem1.java
+++ src/Code/Peoblem1.java
@@ -1,35 +1,20 @@
 package Code;
 
 public class Peoblem1 {
-    // 测试一下
-    public static class Singleton {
-        private final static Singleton INSTANCE = new Singleton();
 
-        private Singleton() {
-
-        }
-
-        public static Singleton getInstance() {
-            return INSTANCE;
-        }
-    }
-
-    /**
-     * 单例模式，饿汉式，线程不安全
-     */
-    public static class Singleton2 {
-        private static Singleton2 instance = null;
-
-        private Singleton2() {
-
-        }
-
-        public static Singleton2 getInstance() {
-            if (instance == null) {
-                instance = new Singleton2();
+    public boolean Find(int target, int [][] array) {
+        int rows = array.length;
+        int cols = array[0].length;
+        int i = rows - 1;
+        int j = 0;
+        while (array != null && i >= 0 && j <= cols -1) {
+            if (target == array[i][j]) { return true;}
+            if (target > array[i][j]) {
+                j++;
+            } else {
+                i--;
             }
-
-            return instance;
         }
+        return false;
     }
 }
